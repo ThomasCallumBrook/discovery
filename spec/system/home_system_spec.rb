96b114de673fc0c36cabc88f3be62cd85d225393
Thomas Callum Brook <thomascallumbrook@gmail.com>
2018-06-22 10:45:27 +0100

    Fix bug where country name with space broke explored country list

--- spec/system/home_system_spec.rb
+++ spec/system/home_system_spec.rb
@@ -1,14 +1,19 @@
 require "rails_helper"
-RSpec.describe "Notes", :type => :system do
+RSpec.describe "Discovery", :type => :system do
   let(:valid_user) {
     User.create(name: "test", email: "test@test.com", password: "testing")
   }
+  let(:valid_country) {
+    Country.create!(bounds: '33.902427,67.84322900000001', name: 'Afghanistan', user: valid_user)
+  }
+
   def login_as(user)
     visit '/'
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'testing'
     click_button('Log in')
   end
+
   it "redirects to sign_in when not logged in", js: true do
     visit '/'
     expect(page).to have_text 'Log in'
@@ -18,7 +23,13 @@ RSpec.describe "Notes", :type => :system do
     visit '/'
     expect(page).to have_text 'EXPLORE, MORE'
   end
-
+  it "shows lsit of countries for users/id/countries", js: true do
+    login_as(valid_user)
+    page.execute_script('window.scrollTo(0,100000)')
+    find(".cntrys-btn", match: :first).click
+    take_screenshot
+    visit "/users/#{valid_user.id}/countries"
+  end
 
 end
 # find('#note-content').native.clear
